2cb9c826758a6df49972d544256aa765cef20ec4
Teerayut_Mix <mix11503@gmail.com>
2018-04-28 15:29:02 +0700

    update complete flow

--- src/main/java/com/int492/cifarclssfr/cifarwebservice/CifarwebserviceApplication.java
+++ src/main/java/com/int492/cifarclssfr/cifarwebservice/CifarwebserviceApplication.java
@@ -2,11 +2,27 @@ package com.int492.cifarclssfr.cifarwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CifarwebserviceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CifarwebserviceApplication.class, args);
-	}
+public class CifarwebserviceApplication extends SpringBootServletInitializer {
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CifarwebserviceApplication.class);
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(CifarwebserviceApplication.class, args);
+    }
+ 
 }
+
+//@SpringBootApplication
+//public class CifarwebserviceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(CifarwebserviceApplication.class, args);
+//	}
+//}
--- src/main/java/com/int492/cifarclssfr/cifarwebservice/controller/CIFARClassifiedController.java
+++ src/main/java/com/int492/cifarclssfr/cifarwebservice/controller/CIFARClassifiedController.java
@@ -9,15 +9,17 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 /**
  *
@@ -26,9 +28,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api")
 public class CIFARClassifiedController {
-    
+
     @Autowired
     private Environment env;
+
+    @Autowired
+    private StringRedisTemplate template;
 
     @RequestMapping(value = "status", method = RequestMethod.GET)
     @ResponseBody
@@ -36,22 +41,80 @@ public class CIFARClassifiedController {
         return "api status OK!!";
     }
 
+    @RequestMapping(value = "checkfile", method = RequestMethod.GET)
+    @ResponseBody
+    public File[] checkfile() {
+        String path = env.getRequiredProperty("target.file.save");
+        System.out.println("PATH ::::::::::::: " + path);
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+        return listOfFiles;
+    }
+
     @RequestMapping(value = "classified", method = RequestMethod.POST)
     @ResponseBody
-    public String classifiedCIFAR(HttpSession session, InputStream dataStream) throws IOException {
+    public String classifiedCIFAR(HttpSession session, InputStream dataStream) throws IOException, InterruptedException {
         String sessionId = session.getId();
-        String outputPath = env.getRequiredProperty("target.file.save")+sessionId+".png";
-        File targetFile = new File(outputPath);
-        //Path path = Files.createTempFile("C:/Users/Mix/Desktop/tmp/pic", ".png");
-        try (FileOutputStream out = new FileOutputStream(targetFile)) {
-        byte[] buffer = new byte[1024]; 
-        int len; 
-        while ((len = dataStream.read(buffer)) != -1) { 
-            out.write(buffer, 0, len); 
-        }
-    } catch (Exception e) {
-        // TODO: handle exception
+        String outputPath = env.getRequiredProperty("target.file.save");
+        String fileName = sessionId + System.currentTimeMillis() + ".png";
+
+        saveFile(dataStream, outputPath, fileName);
+        recordToRedis(sessionId, outputPath, fileName);
+        
+        String result = null;
+        int counter = 0;
+        do{
+            if(counter >= 10){
+                throw new HttpClientErrorException(HttpStatus.REQUEST_TIMEOUT);
+            }
+            Thread.sleep(3000);
+            System.out.println("-------------------CHECK REDIS-------------------");
+            result = getResultClassified(sessionId);
+            System.out.println("Result : "+ result);
+            counter++;
+        }while(result == null);
+
+        return result;
     }
-        return sessionId+", Commplete";
+
+    private void saveFile(InputStream dataStream, String outputPath, String fileName) {
+        File targetFile = new File(outputPath +"/"+ fileName);
+        File f = new File(outputPath);
+        if (f.exists()) {
+            System.out.println("-------------------------------PATH EXIST------------------------");
+        }else{
+            System.out.println("-------------------------------PATH NOT EXIST------------------------");
+            f.mkdirs();
+        }
+        try (FileOutputStream out = new FileOutputStream(targetFile)) {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = dataStream.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    private void recordToRedis(String sessionId, String outputPath, String fileName) {
+        ValueOperations<String, String> ops = this.template.opsForValue();
+        String key = "@" + sessionId;
+        String value = outputPath +"/"+ fileName;
+        ops.set(key, value);
+    }
+    
+    private String getResultClassified(String sessionId){
+        ValueOperations<String, String> ops = this.template.opsForValue();
+        String result = ops.get(sessionId);
+        return result;
     }
 }
